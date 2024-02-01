063faf19b4df7166bb471435c04fdc49084523e7
90PlusWorks <102657193+90PlusWorks@users.noreply.github.com>
2024-02-01 10:47:57 +0200

    Create opencsv.js

---
+++ js/opencsv.js
@@ -0,0 +1,16 @@
+let reader = new FileReader();
+//let is more restricted {let ... } scope is inside{}
+    picker = document.getElementById("loadTransactions");
+// (A) READ CSV ON FILE PICK or LOAD or READ
+picker.onchange = () => reader.readAsText(picker.files[0]);
+// (B) READ CSV & GENERATE hashes
+let hash = [];
+reader.onloadend = () => {
+    var ij = 0;
+  for (let row of CSV.parse(reader.result)) {
+      hash[ij] = hash(col);      
+    }
+  ij = ij + 1;
+  }
+// (C) SAVE hashes
+
